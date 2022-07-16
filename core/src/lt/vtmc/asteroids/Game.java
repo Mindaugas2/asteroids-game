2ad31905f23e5cad2ef558fa7a5712c7fbffce43
Julius <jzabulenas@protonmail.com>
2022-07-16 17:10:13 +0300

    make game screen black, as well as add a fixed window size

--- core/src/lt/vtmc/asteroids/Game.java
+++ core/src/lt/vtmc/asteroids/Game.java
@@ -1,31 +1,32 @@
 package lt.vtmc.asteroids;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
 public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+	public static int WIDTH;
+	public static int HEIGHT;
+
+	public static OrthographicCamera cam;
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+		WIDTH = Gdx.graphics.getWidth();
+		HEIGHT = Gdx.graphics.getHeight();
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render() {
+//		clear screen to black
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	}
-	
+
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void dispose() {
+
 	}
 }
--- desktop/src/lt/vtmc/asteroids/DesktopLauncher.java
+++ desktop/src/lt/vtmc/asteroids/DesktopLauncher.java
@@ -2,14 +2,17 @@ package lt.vtmc.asteroids;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import lt.vtmc.asteroids.Game;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+
 		config.setForegroundFPS(60);
-		config.setTitle("Asteroids game");
+		config.setTitle("Asteroids");
+
+		config.setWindowedMode(500, 400);
+
 		new Lwjgl3Application(new Game(), config);
 	}
 }
